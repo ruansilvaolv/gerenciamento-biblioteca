09088edb54aed820fd63595a4cc714e2c844fc6e
Ruan Oliveira <ruansilvaolv@gmail.com>
2025-08-27 18:14:41 -0300

    - Feat: Implementation of the LivroFactory class, maintaining the factory   pattern.

---
+++ Domain/Factories/LivroFactory.cs
@@ -0,0 +1,12 @@
+using GerenciamentoBiblioteca.Domain.Entities;
+
+namespace GerenciamentoBiblioteca.Domain.Factories
+{
+    public class BookFactory
+    {
+        public static Book CreateBook(string bookName, string author, string isbn, int publicationYear)
+        {
+            return new Book(bookName, author, isbn, publicationYear);
+        }
+    }
+}
