278627d8b93546de56dd4aa6cd9251b28240ec3a
Ruan Oliveira <ruansilvaolv@gmail.com>
2025-09-17 17:16:16 -0300

    - Refactor: Refactor the UIHelper class into the IUIHelper interface, keeping   the same implemented logic.

---
+++ UI/IUIHelper.cs
@@ -0,0 +1,8 @@
+namespace LibraryManagement.UI
+{
+    public interface IUIHelper
+    {
+        string ReadString(string prompt);
+        int ReadInt(int prompt);
+    }
+}
--- UI/UIHelper.cs
+++
@@ -1,8 +0,0 @@
-namespace LibraryManagement.UI
-{
-    public abstract class UIHelper
-    {
-        public abstract string ReadString(string prompt);
-        public abstract int ReadInt(int prompt);
-    }
-}
