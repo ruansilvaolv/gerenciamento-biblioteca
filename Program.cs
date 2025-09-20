4f4e704c5098b655090640439229e169af65ec52
Ruan Oliveira <ruansilvaolv@gmail.com>
2025-09-20 10:34:59 -0300

    -Refactor: Refactors the logic of the Program class, initializing the consoleUI object and executing its Run method to start the application.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using LibraryManagement.Services;
+using LibraryManagement.UI;
+
 namespace LibraryManagement;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var ui = new ConsoleUIHelper();
+        var library = new Library(ui);
+        var consoleUI = new LibraryConsoleUI(library, ui);
 
+        consoleUI.Run();
     }
 }
